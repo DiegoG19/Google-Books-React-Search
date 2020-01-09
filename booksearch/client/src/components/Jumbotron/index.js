4c6ab739fe5e54fbefd1ca5087c5ecf1de964e67
Diego G <gd369234@gmail.com>
2020-01-09 01:17:35 -0600

    final build good results and fix the issues in mongoDB

---
+++ booksearch/client/src/components/Jumbotron/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Jumbotron() {
+    return (
+        <div className="jumbotron text-center">
+            <h1>Google Books Api App</h1>
+            <h3>Search for books and Save them for later </h3>
+        </div>
+    );
+}
+
+export default Jumbotron;
